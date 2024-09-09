7982e266fb09c7a55b98efccaf803ab1341727a3
Ken D Teasley <bigtee32@hotmail.com>
2024-09-08 22:22:18 -0400

    finally

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signin from './screens/Signin';
 import Search from './screens/Search';
 import BookDetails from './screens/BookDetails';
 import Bookshelf from './screens/Bookshelf';
-import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 
 const App: React.FC = () => {
   return (
+    <div>
+      <h1>Test Render</h1>
+    
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Signin />} />
-        <Route element={<ProtectedRoute />}/>
+        <Route element={<ProtectedRoute />}>
           <Route path="/search" element={<Search />} />
           <Route path="/book/:bookId" element={<BookDetails />} />
           <Route path="/bookshelf" element={<Bookshelf />} />
+        </Route>
       </Routes>
-      
     </Router>
+    </div>
   );
 };
+
 export default App;
